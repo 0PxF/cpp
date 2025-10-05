d01899280e1508a459a602f79174924d069d2fbf
0PxF <praveentoppo470@gmail.com>
2025-10-05 07:36:59 +0000

    Create input.cpp
    Program to print an integer entered by the user.

---
+++ intro/input.cpp
@@ -0,0 +1,13 @@
+// Program to print an integer entered by the user.
+#include <iostream>
+using namespace std;
+//Main Function
+int main() {
+//Variables
+int x;
+//Input
+cout << "Enter a number(Integer): " << endl;
+cin >> x;
+//Output
+cout << "Entered Number:" << x << endl;
+}
