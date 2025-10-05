518679aeb25a0844757348fa7f71206ee25e3e9e
0PxF <praveentoppo470@gmail.com>
2025-10-05 05:34:48 +0000

    Create sum.cpp
    Basic Program to sum two numbers using variables

---
+++ intro/sum.cpp
@@ -0,0 +1,15 @@
+/*
+Program to sum the two variables
+  */
+#include <iostream>
+using namespace std;
+//Main Function
+int main(){
+  //Variables
+  int x=5,y=10;
+  int sum=0;
+  //Sum
+  sum=x+y;
+  //Printing Sum
+cout << "Sum:" << sum << endl;
+  }
